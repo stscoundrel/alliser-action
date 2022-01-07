9e7970097ad0b57acb417baae020c0193c839d09
Sampo Silvennoinen <silvennoinen.sampo@gmail.com>
2022-01-07 16:39:15 +0200

    Add action src

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,13 @@
-export const example = (): string => 'test string';
+import core from '@actions/core';
+import alliser from 'alliser';
 
-export default {
-  example,
-};
+const parseCommaSeparated = (input: string): string[] => input.split(',');
+
+try {
+  const extensions = parseCommaSeparated(core.getInput('extensions'));
+  const folders = parseCommaSeparated(core.getInput('folders'));
+
+  alliser.validate(extensions, folders);
+} catch (error) {
+  core.setFailed(error.message);
+}
